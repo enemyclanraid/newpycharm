2ed1368ff9ad52b136f5dd71af11aa183a411f21
George <georgetokach@gmail.com>
2024-12-31 12:59:33 +0300

    first run

---
+++ main.py
@@ -0,0 +1,55 @@
+
+
+# print("Hello world")
+# print(type("Hello world"))
+#
+# print(34)
+# print(type(34))
+#
+# print(45.34)
+# print(type(45.34))
+#
+#
+# print(34 + 87)
+# print(34**3)
+# # + - * /
+# # // целочисленное деление
+# # ** степень
+# # ** % остаток.
+#
+#
+# print(3/5)
+# print(6//2)
+#
+# print("Hello" + str(3) + " world!")
+#
+# print("3" + "5")
+
+
+#=========================
+
+# True False
+
+# print(True)
+# print(False)
+
+
+# print(5 > 1)
+# print(3 > 32)
+
+# > < == !=
+
+# 4th program
+
+nstr = 123.456
+
+nstrf = float(nstr)
+
+nstrf *= 10
+
+nstrfi = int(nstrf)
+
+ost = nstrfi % 10
+
+print(ost)
+
